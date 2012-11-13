651d111cf8485e89a849bba93161f909cc46b609
Quintus <quintus@quintilianus.eu>
2012-11-13 22:55:03 +0100

    Use Gtk::Layout + Gtk::ScrolledWindow for the map table widget.
    Gtk::ScrolledWidget provides us with automatic scrolling. This way
    we don’t have to cater for this ourselves, which is a tricky task.

--- lib/open_ruby_rmk/gtk_frontend/widgets/map_table.rb
+++ lib/open_ruby_rmk/gtk_frontend/widgets/map_table.rb
@@ -1,11 +1,16 @@
-class OpenRubyRMK::GTKFrontend::Widgets::MapTable < Gtk::DrawingArea
+class OpenRubyRMK::GTKFrontend::Widgets::MapTable < Gtk::ScrolledWindow
 
   def initialize
     super
     @map = nil
 
-    set_size_request(32, 32)
-    signal_connect(:expose_event, &method(:on_expose))
+    @layout = Gtk::Layout.new
+
+    set_size_request(500, 400)
+    @layout.set_size(800, 800)
+    @layout.signal_connect(:expose_event, &method(:on_expose))
+
+    add(@layout)
   end
 
   def map=(map)
@@ -16,9 +21,17 @@ class OpenRubyRMK::GTKFrontend::Widgets::MapTable < Gtk::DrawingArea
   private
 
   def on_expose(_, event)
-    gc = Gdk::GC.new(window)
+    gc = Gdk::GC.new(@layout.bin_window)
     gc.rgb_fg_color = Gdk::Color.new(50000, 0, 0)
-    window.draw_rectangle(gc, true, 0, 0, allocation.width, allocation.height)
+    gc2 = Gdk::GC.new(@layout.bin_window)
+    gc2.rgb_fg_color = Gdk::Color.new(0, 50000, 0)
+
+    gcs = [gc, gc2].cycle
+
+    0.step(@layout.size[0], 10) do |x|
+      @layout.bin_window.draw_rectangle(gcs.next, true, x, 0, 10, @layout.size[1])
+    end
+
 
     true
   end
