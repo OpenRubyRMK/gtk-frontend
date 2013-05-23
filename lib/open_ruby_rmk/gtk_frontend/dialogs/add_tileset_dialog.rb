82fb6c3fe77a091e613f4b9093b409bf8526f902
Quintus <quintus@quintilianus.eu>
2013-05-23 20:23:57 +0200

    Abstract the resource selection dialog

--- lib/open_ruby_rmk/gtk_frontend/dialogs/add_tileset_dialog.rb
+++ lib/open_ruby_rmk/gtk_frontend/dialogs/add_tileset_dialog.rb
@@ -1,80 +1,32 @@
 # -*- coding: utf-8 -*-
 
-class OpenRubyRMK::GTKFrontend::Dialogs::AddTilesetDialog < Gtk::Dialog
-  include Gtk
-  include R18n::Helpers
-  include OpenRubyRMK::GTKFrontend::Validatable
+class OpenRubyRMK::GTKFrontend::Dialogs::AddTilesetDialog < OpenRubyRMK::GTKFrontend::Dialogs::ResourceSelectionDialog
 
   validate do
     val_error "No tileset selected." if !@directory_tree.selected_path or !@directory_tree.selected_path.file?
   end
 
   def initialize(map)
-    super("Add tileset",
-          $app.mainwindow,
-          Dialog::MODAL | Dialog::DESTROY_WITH_PARENT,
-          [Stock::OK, Dialog::RESPONSE_ACCEPT],
-          [Stock::CANCEL, Dialog::RESPONSE_REJECT])
-
-    set_default_size 500, 300
     @map = map
-
-    create_widgets
-    create_layout
-    setup_event_handlers
+    super("Add tileset", "tilesets", $app.mainwindow)
   end
 
-  # Shows all child widgets, then calls the superclass’
-  # method.
-  def run(*)
-    show_all
-    super
+  def create_preview_widget
+    @image = Image.new
   end
 
-  private
-
-  def create_widgets
-    @directory_tree = OpenRubyRMK::GTKFrontend::Widgets::DirectoryTreeView.new($app.project.paths.data_dir + "tilesets", true)
-    @image          = Image.new
-  end
-
-  def create_layout
-    HBox.new.tap do |hbox|
-      hbox.pack_start(@directory_tree, false)
-      ScrolledWindow.new.tap do |scroller|
-        scroller.add_with_viewport(@image)
-
-        hbox.pack_start(scroller, true)
-      end
-
-      vbox.pack_start(hbox, true)
+  def get_preview_widget
+    ScrolledWindow.new.tap do |scroller|
+      scroller.add_with_viewport(@image)
     end
   end
 
-  def setup_event_handlers
-    signal_connect(:response, &method(:on_response))
-    @directory_tree.signal_connect(:cursor_changed, &method(:on_cursor_changed))
+  def handle_accept(selected_path)
+    tileset = TiledTmx::Tileset.load_xml(selected_path)
+    @map.add_tileset(tileset)
   end
 
-  ########################################
-  # Event handlers
-
-  def on_response(_, res)
-    if res == Gtk::Dialog::RESPONSE_ACCEPT
-      $app.warnbox(validation_summary) and return unless valid?
-
-      tileset = TiledTmx::Tileset.load_xml(@directory_tree.selected_path)
-      @map.add_tileset(tileset)
-    end
-
-    destroy
-  end
-
-  def on_cursor_changed(*)
-    @image.pixbuf = nil
-    return unless @directory_tree.selected_path
-    return unless @directory_tree.selected_path.file?
-
+  def handle_cursor_changed(selected_path)
     tileset = TiledTmx::Tileset.load_xml(@directory_tree.selected_path)
     @image.pixbuf = Gdk::Pixbuf.new(tileset.source.to_s)
   end
---
+++ lib/open_ruby_rmk/gtk_frontend/dialogs/resource_selection_dialog.rb
@@ -0,0 +1,119 @@
+# Generic resource selection dialog. This class needs to be subclassed
+# and the following methods need to be overridden:
+#
+# [initialize]
+#   Ensure to call +super+ with +resource_dir+ set to the
+#   subpath of the resource you want to query (e.g. "tilesets").
+# [create_preview_widget]
+#   Create the widget for the preview of the resource.
+# [get_preview_widget]
+#   Return the widget for the preview of the resource. It will
+#   be added to a Gtk::HBox via #pack_start with the second
+#   parameter set to true.
+# [handle_accept]
+#   User clicked the OK button. This method gets passed the selected
+#   path as a Pathname instance.
+# [handle_cursor_changed]
+#   User selected another path from thre tree view. This method gets
+#   passed the selected path as a Pathname instance.
+# [setup_signal_handlers]
+#   Not required to override, but may be useful. Call +super+ (without
+#   arguments) to ensure the main signal handlers also get set up.
+#
+# Additionally, this class mixes in the Validatable module and
+# the OK button signal handler queries the validations. You can therefore
+# set up your +validate+ blocks as expected
+class OpenRubyRMK::GTKFrontend::Dialogs::ResourceSelectionDialog < Gtk::Dialog
+  include Gtk
+  include R18n::Helpers
+  include OpenRubyRMK::GTKFrontend::Validatable
+
+  # Create a new generic resource selection dialog.
+  # == Parameters
+  # [title]
+  #   Title for the dialog.
+  # [resource_dir]
+  #   Resource *subdirectory* relative to the main resources path
+  #   to allow selection from.
+  # [parent]
+  #   Parent window to be modal to. You can set this to $app.mainwindow.
+  # == Return value
+  # The newly created instance.
+  def initialize(title, resource_dir, parent)
+    super(title,
+          parent,
+          Dialog::MODAL | Dialog::DESTROY_WITH_PARENT,
+          [Stock::OK, Dialog::RESPONSE_ACCEPT],
+          [Stock::CANCEL, Dialog::RESPONSE_REJECT])
+
+    @resource_dir = resource_dir
+    set_default_size 500, 300
+
+    create_widgets
+    create_layout
+    setup_event_handlers
+  end
+
+  # Run the dialog showing all its child widgets.
+  def run(*)
+    show_all
+    super
+  end
+
+  private
+
+  def create_widgets
+    @directory_tree = OpenRubyRMK::GTKFrontend::Widgets::DirectoryTreeView.new($app.project.paths.data_dir + @resource_dir, true)
+    create_preview_widget
+  end
+
+  def create_layout
+    HBox.new.tap do |hbox|
+      hbox.pack_start(@directory_tree, false)
+      hbox.pack_start(get_preview_widget, true)
+
+      vbox.pack_start(hbox, true)
+    end
+  end
+
+  def setup_event_handlers
+    signal_connect(:response, &method(:on_response))
+    @directory_tree.signal_connect(:cursor_changed, &method(:on_cursor_changed))
+  end
+
+  ########################################
+  # Event handlers
+
+  def on_response(_, res)
+    if res == Gtk::Dialog::RESPONSE_ACCEPT
+      $app.warnbox(validation_summary) and return unless valid?
+      handle_accept(@directory_tree.selected_path)
+    end
+
+    destroy
+  end
+
+  def on_cursor_changed(*)
+    return unless @directory_tree.selected_path
+    return unless @directory_tree.selected_path.file?
+
+    handle_cursor_changed(@directory_tree.selected_path)
+  end
+
+  def create_preview_widget
+    # Must be overridden
+  end
+
+  def get_preview_widget
+    # Must be overridden
+  end
+
+  def handle_accept
+    # Must be overridden
+  end
+
+  def handle_cursor_changed
+    # Must be overridden
+  end
+
+end
