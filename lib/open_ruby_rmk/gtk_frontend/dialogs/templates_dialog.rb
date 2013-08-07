a5e0ba8082416e7c7d6a514cedce3edf11ed41f7
Quintus <quintus@quintilianus.eu>
2013-08-07 16:55:43 +0200

    Add widgets for template parameters

--- lib/open_ruby_rmk/gtk_frontend/dialogs/templates_dialog.rb
+++ lib/open_ruby_rmk/gtk_frontend/dialogs/templates_dialog.rb
@@ -2,6 +2,7 @@
 class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
   include Gtk
   include R18n::Helpers
+  include OpenRubyRMK::GTKFrontend::Helpers::Icons
 
   def initialize
     super("Templates",
@@ -29,6 +30,9 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
 
   def create_widgets
     @list = TreeView.new(ListStore.new(String, OpenRubyRMK::Backend::Template))
+    @codepages = Notebook.new
+    @add_template_button = Button.new
+    @del_template_button = Button.new
 
     @list.rules_hint = true
     @list.headers_visible = false
@@ -37,12 +41,30 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
     @listrenderer.editable = true
     @list.append_column(TreeViewColumn.new("", @listrenderer, text: 0))
 
-    @codepages = Notebook.new
+    @add_template_button.add(icon_image("ui/list-add.png", width: 16))
+    @del_template_button.add(icon_image("ui/list-remove.png", width: 16))
+
+    @sourceviews = []
+    @paraviews = []
   end
 
   def create_layout
     HBox.new.tap do |hbox|
-      hbox.pack_start(@list, false, false)
+      hbox.spacing = $app.space
+
+      VBox.new.tap do |vbox2|
+        vbox2.pack_start(@list, true, true)
+
+        HBox.new.tap do |hbox2|
+          hbox2.pack_start(@add_template_button, false, false)
+          hbox2.pack_end(@del_template_button, false, false)
+
+          vbox2.pack_start(hbox2, false, false)
+        end
+
+        hbox.pack_start(vbox2, false, false)
+      end
+
       hbox.pack_start(@codepages, true, true)
 
       vbox.pack_start(hbox, true, true)
@@ -52,6 +74,8 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
   def setup_event_handlers
     signal_connect(:response, &method(:on_response))
     @list.signal_connect(:cursor_changed, &method(:on_list_cursor_changed))
+    @add_template_button.signal_connect(:clicked, &method(:on_add_template_button_clicked))
+    @del_template_button.signal_connect(:clicked, &method(:on_del_template_button_clicked))
   end
 
   ########################################
@@ -69,14 +93,47 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
 
     template = current_list_iter[1]
     template.pages.each do |page|
+      # Make the widgets
       sourceview = OpenRubyRMK::GTKFrontend::Widgets::RubySourceView.new
-      sourceview.buffer.text = page.code
+      paraview   = TreeView.new(ListStore.new(String, String, String, OpenRubyRMK::Backend::Template::Parameter))
 
-      @codepages.append_page(sourceview,
-                             Label.new(sprintf(t.dialogs.template_event.labels.page,
-                                               :num => page.number)))
+      # Tell the parameter list what to display
+      paraview.append_column(TreeViewColumn.new("Name", CellRendererText.new, text: 0))
+      paraview.append_column(TreeViewColumn.new("Type", CellRendererText.new, text: 1))
+      paraview.append_column(TreeViewColumn.new("Default value", CellRendererText.new, text: 2))
+
+      # Fill in parameters and code for this template page
+      sourceview.buffer.text = page.code
+      page.parameters.each do |param|
+        row = paraview.model.append
+        row[0] = param.name
+        row[1] = param.type
+        row[2] = param.default_value || "(required)"
+      end
+
+      # Add the widgets to the notebook page
+      VBox.new.tap do |vbox2|
+        scroller1 = ScrolledWindow.new
+        scroller2 = ScrolledWindow.new
+        scroller1.add(paraview)
+        scroller2.add(sourceview)
+        vbox2.pack_start(scroller1, false)
+        vbox2.pack_start(scroller2, true, true)
+
+        @codepages.append_page(vbox2,
+                               Label.new(sprintf(t.dialogs.template_event.labels.page,
+                                                 :num => page.number)))
+      end
     end
     @codepages.show_all
+  end
+
+  def on_add_template_button_clicked(*)
+    
+  end
+
+  def on_del_template_button_clicked(*)
+
   end
 
   ########################################
