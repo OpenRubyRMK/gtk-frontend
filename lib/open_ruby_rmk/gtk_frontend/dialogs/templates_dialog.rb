74ba265b7e62ecee9112e4af124a3f18b69143ec
Quintus <quintus@quintilianus.eu>
2013-08-08 17:51:41 +0200

    Add buttons for deleting and inserting template pages

--- lib/open_ruby_rmk/gtk_frontend/dialogs/templates_dialog.rb
+++ lib/open_ruby_rmk/gtk_frontend/dialogs/templates_dialog.rb
@@ -33,6 +33,8 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
     @codepages = Notebook.new
     @add_template_button = Button.new
     @del_template_button = Button.new
+    @add_page_button     = Button.new
+    @del_page_button     = Button.new
 
     @list.rules_hint = true
     @list.headers_visible = false
@@ -43,6 +45,8 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
 
     @add_template_button.add(icon_image("ui/list-add.png", width: 16))
     @del_template_button.add(icon_image("ui/list-remove.png", width: 16))
+    @add_page_button.add(icon_image("ui/list-add.png", width: 16))
+    @del_page_button.add(icon_image("ui/list-remove.png", width: 16))
 
     @sourceviews = []
     @paraviews = []
@@ -65,7 +69,17 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
         hbox.pack_start(vbox2, false, false)
       end
 
-      hbox.pack_start(@codepages, true, true)
+      VBox.new.tap do |vbox2|
+        vbox2.pack_start(@codepages, true, true)
+        HBox.new.tap do |hbox2|
+          hbox2.pack_start(@add_page_button, false, false)
+          hbox2.pack_start(@del_page_button, false, false, $app.space)
+
+          vbox2.pack_start(hbox2, false, false)
+        end
+
+        hbox.pack_start(vbox2, true, true)
+      end
 
       vbox.pack_start(hbox, true, true)
     end
@@ -76,6 +90,8 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
     @list.signal_connect(:cursor_changed, &method(:on_list_cursor_changed))
     @add_template_button.signal_connect(:clicked, &method(:on_add_template_button_clicked))
     @del_template_button.signal_connect(:clicked, &method(:on_del_template_button_clicked))
+    @add_page_button.signal_connect(:clicked, &method(:on_add_page_button_clicked))
+    @del_page_button.signal_connect(:clicked, &method(:on_del_page_button_clicked))
   end
 
   ########################################
@@ -93,36 +109,7 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
 
     template = current_template_list_iter[1]
     template.pages.each do |page|
-      # Make the widgets
-      sourceview = OpenRubyRMK::GTKFrontend::Widgets::RubySourceView.new
-      paraview   = TreeView.new(ListStore.new(String, String, OpenRubyRMK::Backend::Template::Parameter))
-
-      # Tell the parameter list what to display
-      paraview.append_column(TreeViewColumn.new("Name", CellRendererText.new, text: 0))
-      paraview.append_column(TreeViewColumn.new("Default value", CellRendererText.new, text: 1))
-
-      # Fill in parameters and code for this template page
-      sourceview.buffer.text = page.code
-      page.parameters.each do |param|
-        row = paraview.model.append
-        row[0] = param.name
-        row[1] = param.required? ? "(required)" : param.default_value
-        row[2] = param
-      end
-
-      # Add the widgets to the notebook page
-      VBox.new.tap do |vbox2|
-        scroller1 = ScrolledWindow.new
-        scroller2 = ScrolledWindow.new
-        scroller1.add(paraview)
-        scroller2.add(sourceview)
-        vbox2.pack_start(scroller1, false)
-        vbox2.pack_start(scroller2, true, true)
-
-        @codepages.append_page(vbox2,
-                               Label.new(sprintf(t.dialogs.template_event.labels.page,
-                                                 :num => page.number)))
-      end
+      insert_page(template, page)
     end
     @codepages.show_all
   end
@@ -141,6 +128,28 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
     @list.model.remove(iter)
   end
 
+  def on_add_page_button_clicked(*)
+    return unless iter = current_template_list_iter # Single = intended
+
+    page = OpenRubyRMK::Backend::Template::TemplatePage.new(0)
+    page.instance_eval do
+      parameter "myparameter"
+      code('puts "Hello World!"')
+    end
+
+    iter[1].insert_page(@codepages.page + 1, page)
+
+    insert_page(iter[1], page, page.number)
+    @codepages.show_all
+  end
+
+  def on_del_page_button_clicked(*)
+    return unless iter = current_template_list_iter # Single = intended
+
+    iter[1].pages.delete_at(@codepages.page)
+    @codepages.remove_page(@codepages.page)
+  end
+
   ########################################
   # Helpers
 
@@ -150,6 +159,39 @@ class OpenRubyRMK::GTKFrontend::Dialogs::TemplatesDialog < Gtk::Dialog
 
     row[0] = tname
     row[1] = template
+  end
+
+  def insert_page(template, page, index = -1)
+    # Make the widgets
+    sourceview = OpenRubyRMK::GTKFrontend::Widgets::RubySourceView.new
+    paraview   = TreeView.new(ListStore.new(String, String, OpenRubyRMK::Backend::Template::Parameter))
+
+    # Tell the parameter list what to display
+    paraview.append_column(TreeViewColumn.new("Name", CellRendererText.new, text: 0))
+    paraview.append_column(TreeViewColumn.new("Default value", CellRendererText.new, text: 1))
+
+    # Fill in parameters and code for this template page
+    sourceview.buffer.text = page.code
+    page.parameters.each do |param|
+      row = paraview.model.append
+      row[0] = param.name
+      row[1] = param.required? ? "(required)" : param.default_value
+      row[2] = param
+    end
+
+    # Add the widgets to the notebook page
+    VBox.new.tap do |vbox2|
+      scroller1 = ScrolledWindow.new
+      scroller2 = ScrolledWindow.new
+      scroller1.add(paraview)
+      scroller2.add(sourceview)
+      vbox2.pack_start(scroller1, false)
+      vbox2.pack_start(scroller2, true, true)
+
+      @codepages.insert_page(index, vbox2,
+                             Label.new(sprintf(t.dialogs.template_event.labels.page,
+                                               :num => page.number)))
+    end
   end
 
   def current_template_list_iter
